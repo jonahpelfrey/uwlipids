ada0ef98330bf7816607b49835ae158b2e74a092
jonahpelfrey <jsp916@gmail.com>
2017-11-05 13:13:57 -0600

    added style class for age inputs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,14 +4,18 @@ import { Patient, Status } from '../../models/patient';
 
 @Component({
 	selector: 'page-home',
-  	templateUrl: 'home.html'
+  	templateUrl: 'home.html',
 })
 
 export class HomePage implements OnInit{
 
 	age: number = 50;
-	showFasting: boolean = false;
-	showCholesterol: boolean = false;
+	ageSubmitted: boolean = false;
+	ageStyle = {
+		'text-default': true,
+		'text-warning': false,
+		'text-valid': false
+	};
 
  	constructor(public navCtrl: NavController) {
 
@@ -19,6 +23,37 @@ export class HomePage implements OnInit{
 
   	ngOnInit(): void {
 
+  	}
+
+  	validateAge(): void {
+
+  		this.ageSubmitted = true;
+
+  		if(this.age < 2 || this.age >= 25) {
+  			this.ageStyle = {
+  				'text-default': false,
+				'text-warning': true,
+				'text-valid': false
+  			}
+  		}
+  		else {
+  			this.ageStyle = {
+  				'text-default': false,
+				'text-warning': false,
+				'text-valid': true
+  			}
+  		}
+  		
+  	}
+
+  	resetAge(): void {
+  		this.age = 50;
+  		this.ageSubmitted = false;
+  		this.ageStyle = {
+  			'text-default': true,
+  			'text-warning': false,
+  			'text-valid': false
+  		};
   	}
 
 }
